c40b0771852fb266423666a8edf148e79b7cd02d
dilless <bohuang1994@gmail.com>
2022-08-24 12:41:06 +0800

    Added example test cases

---
+++ app/app/calc.py
@@ -0,0 +1,19 @@
+"""
+    calc
+    ~~~
+    Calculator functions
+
+
+    :author: dilless(Huangbo)
+    :date: 2022/8/23
+"""
+
+
+def add(x, y):
+    """ Add x and y and return results. """
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
